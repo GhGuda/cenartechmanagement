2311fb2bdabd8a34e1167be9fd9406203208a1c0
GhGuda <toyboipressure1@gmail.com>
2024-10-01 13:50:45 +0000

    Bug fixes

--- cenartechpro/settings.py
+++ cenartechpro/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = config("SECRET_KEY")
 # DEBUG = config('DEBUG', cast=bool)
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '18.185.219.179', 'localhost', 'cenartechmanagement.onrender.com', 'cenartechmanagement.top']
+ALLOWED_HOSTS = ['www.cenartechmanagement.top', 'cenartechmanagement.top']
 
 
 # Application definition
@@ -54,8 +54,6 @@ INSTALLED_APPS = [
 CSRF_TRUSTED_ORIGINS = [
     'https://cenartechmanagement.top',
     'http://cenartechmanagement.top',
-    'http://www.cenartechmanagement.top',
-    'https://www.cenartechmanagement.top',
 ]
 
 
